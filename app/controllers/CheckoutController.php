b8d4f00f379c27dbf143775daa4ea19f77662a0f
Vũ Thành Công <104372944+vuthanhcong02@users.noreply.github.com>
2023-06-05 15:57:35 +0700

    fix category list navbar

--- app/controllers/CheckoutController.php
+++ app/controllers/CheckoutController.php
@@ -1,7 +1,9 @@
 <?php
-
+require_once 'app/models/Category.php';
 class CheckoutController{
     public function index(){
+        $categoryModel = new Category();
+        $categories  = $categoryModel->getAllCategory();
         include 'app/views/checkout/index.php';
     }
 }
--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -1,7 +1,9 @@
 <?php
-
+require_once 'app/models/Category.php';
 class ContactController{
     public function index (){
+        $categoryModel = new Category();
+        $categories  = $categoryModel->getAllCategory();
         include 'app/views/contact/index.php';
     }
 }
