a05a6d55b4241f21807675158be720b57e4fefd3
Vũ Thành Công <104372944+vuthanhcong02@users.noreply.github.com>
2023-06-16 14:48:46 +0700

    active menu in admin panel

--- public/layouts/admin/sidebar.php
+++ public/layouts/admin/sidebar.php
@@ -1,3 +1,6 @@
+<?php
+$currentUrl = basename($_SERVER['REQUEST_URI']);
+?>
 <!-- Top container -->
 <div class="w3-bar w3-top w3-black w3-large" style="z-index:4">
   <button class="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onclick="w3_open();"><i class="fa fa-bars"></i>  Menu</button>
@@ -19,16 +22,34 @@
     <h5>Dashboard</h5>
   </div>
   <div class="w3-bar-block">
-    <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i>  Close Menu</a>
-    <a href="index.php?controller=admin&action=index" class="w3-bar-item w3-button w3-padding w3-blue"><i class="fa fa-users fa-fw"></i>  Overview</a>
-    <a href="index.php?controller=adminproduct&action=index" class="w3-bar-item w3-button w3-padding"><i class="fa fa-eye fa-fw"></i>  Product</a>
-    <a href="index.php?controller=admincategory&action=index" class="w3-bar-item w3-button w3-padding"><i class="fa fa-users fa-fw"></i>  Category</a>
-    <a href="index.php?controller=adminorder&action=index" class="w3-bar-item w3-button w3-padding"><i class="fa fa-shopping-basket fa-fw"></i>  Orders</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bell fa-fw"></i>  News</a>
-    <a href="index.php?controller=adminaccount&action=index" class="w3-bar-item w3-button w3-padding"><i class="fa fa-users fa-fw"></i>  Users</a>
-    <!-- <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-envelope fa-fw"></i> Messenger</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bank fa-fw"></i>  General</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-history fa-fw"></i>  History</a> -->
+
+    <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu">
+      <i class="fa fa-remove fa-fw"></i> Close Menu
+    </a>
+
+    <a href="index.php?controller=admin&action=index" class="w3-bar-item w3-button w3-padding <?php if ($currentUrl == 'index.php?controller=admin&action=index') echo 'w3-blue'; ?>">
+      <i class="fa fa-users fa-fw"></i> Overview
+    </a>
+
+    <a href="index.php?controller=adminproduct&action=index" class="w3-bar-item w3-button w3-padding <?php if ($currentUrl == 'index.php?controller=adminproduct&action=index') echo 'w3-blue'; ?>">
+      <i class="fa fa-eye fa-fw"></i> Product
+    </a>
+
+    <a href="index.php?controller=admincategory&action=index" class="w3-bar-item w3-button w3-padding <?php if ($currentUrl == 'index.php?controller=admincategory&action=index') echo 'w3-blue'; ?>">
+      <i class="fa fa-users fa-fw"></i> Category
+    </a>
+
+    <a href="index.php?controller=adminorder&action=index" class="w3-bar-item w3-button w3-padding <?php if ($currentUrl == 'index.php?controller=adminorder&action=index') echo 'w3-blue'; ?>">
+      <i class="fa fa-shopping-basket fa-fw"></i> Orders
+    </a>
+
+    <a href="index.php?controller=adminaccount&action=index" class="w3-bar-item w3-button w3-padding <?php if ($currentUrl == 'index.php?controller=adminaccount&action=index') echo 'w3-blue'; ?>">
+      <i class="fa fa-users fa-fw"></i> Users
+    </a>
+
+    <a href="#" class="w3-bar-item w3-button w3-padding <?php if ($currentUrl == '#') echo 'w3-blue'; ?>">
+      <i class="fa fa-bell fa-fw"></i> News
+    </a>
     <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-sign-out fa-fw"></i> Log out</a>
   </div>
 </nav>
@@ -38,4 +59,3 @@
   <header class="w3-container" style="padding-top:22px">
     <h5><b><i class="fa fa-dashboard"></i> My Dashboard</b></h5>
   </header>
-
